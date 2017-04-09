dcc48216ffa08c54aaf98f34702626d9cc86028f
Jonas Wingård <jonas.wingard@netrelations.com>
2017-04-09 15:43:01 +0200

    removed register route

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -8,26 +8,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/register', function(req, res) {
-    res.render('register', { });
-});
-
-router.post('/register', function(req, res, next) {
-    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
-        if (err) {
-          return res.render('register', { error : err.message });
-        }
-
-        passport.authenticate('local')(req, res, function () {
-            req.session.save(function (err) {
-                if (err) {
-                    return next(err);
-                }
-                res.redirect('/');
-            });
-        });
-    });
-});
+// router.get('/register', function(req, res) {
+//     res.render('register', { });
+// });
+//
+// router.post('/register', function(req, res, next) {
+//     Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+//         if (err) {
+//           return res.render('register', { error : err.message });
+//         }
+//
+//         passport.authenticate('local')(req, res, function () {
+//             req.session.save(function (err) {
+//                 if (err) {
+//                     return next(err);
+//                 }
+//                 res.redirect('/');
+//             });
+//         });
+//     });
+// });
 
 router.get('/login', function(req, res) {
     res.render('login', { user : req.user });
