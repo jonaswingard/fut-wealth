dc6ac02d44449aa571f22ef0e7823a699d09408d
Jonas Wingård <jonas.wingard@gmail.com>
2017-04-24 17:31:34 +0200

    removed clean job

--- api/controllers/trending.js
+++ api/controllers/trending.js
@@ -15,10 +15,4 @@ router.get('/latest', auth.isAuthenticated, (req, res, next) => {
   });
 });
 
-router.get('/clean', auth.isAuthenticated, (req, res, next) => {
-  Trending.clean().then(result => {
-    res.send(result);
-  });
-});
-
 module.exports = router;
